5cad204d7ddf3a1480d69a253d2e953514a7f49c
v.kabancov <v.kabantsov@city-mobil.ru>
2022-03-20 19:15:45 +0300

    max-profit (2 pointers solution)

--- leet-code/[repeat]max-profit.js
+++ leet-code/[repeat]max-profit.js
@@ -12,6 +12,27 @@ const maxProfit = function (prices) {
   }, 0)
 }
 
-console.log(maxProfit([7, 1, 5, 3, 6, 4])) // 5
-console.log(maxProfit([7, 6, 4, 3, 1])) // 0
+const maxProfitTwoPointers = (prices) => {
+  let maxProfit = 0
+  let left = 0
+  let right = 1
 
+  while (right < prices.length) {
+    if (prices[left] < prices[right]) {
+      const profit = prices[right] - prices[left]
+
+      maxProfit = Math.max(maxProfit, profit)
+      console.log({ profit })
+    } else {
+      left = right
+    }
+
+    right++
+  }
+
+  return maxProfit
+}
+
+// console.log(maxProfitTwoPointers([7, 1, 5, 3, 6, 4])) // 5
+// console.log(maxProfitTwoPointers([7, 6, 4, 3, 1])) // 0
+console.log(maxProfitTwoPointers([1, 2])) // 1
